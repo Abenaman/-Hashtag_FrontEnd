fb99dd16761443b8fa9be33f4030a2c2f9bfb95a
Abenezer Berhanu <abenagood5@gmail.com>
2019-02-19 00:24:59 +0200

    Updates

---
+++ precache-manifest.f93d8bdb730fa8a0ebffb08c0e15933d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "13f73dbdd3a645fd8389",
+    "url": "/treadinghashtag_front_end/static/js/runtime~main.13f73dbd.js"
+  },
+  {
+    "revision": "f2a843be91f3e14dc51c",
+    "url": "/treadinghashtag_front_end/static/js/main.f2a843be.chunk.js"
+  },
+  {
+    "revision": "5033df8c04c74cfef553",
+    "url": "/treadinghashtag_front_end/static/js/2.5033df8c.chunk.js"
+  },
+  {
+    "revision": "f2a843be91f3e14dc51c",
+    "url": "/treadinghashtag_front_end/static/css/main.75a72ba9.chunk.css"
+  },
+  {
+    "revision": "5033df8c04c74cfef553",
+    "url": "/treadinghashtag_front_end/static/css/2.9193d666.chunk.css"
+  },
+  {
+    "revision": "31ee943be23ae1c31b93479789859863",
+    "url": "/treadinghashtag_front_end/index.html"
+  }
+];
